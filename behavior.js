1d61974aed9c00bfea3dc736b5a9b7aa70021163
Pandark <apache.kof@gmail.com>
2012-12-12 05:45:46 +0100

    html5, css3, vanilla js and webL10n.js

--- behavior.js
+++ behavior.js
@@ -1,112 +1,95 @@
-$(document).ready(function(){
-    var init,
-        activeStepNum,
-        updateActiveStep,
-        getStepNumber,
-        getStepID,
-        canHazLocalStorage;
-    
-    init = function() {
-        console.log("I'm alive!");
-        canHazLocalStorage = supports_html5_storage();
-        activeStepNum = 1;
-        
-        if (canHazLocalStorage){
-            console.log('HTML 5 storage is go');
-            // default is 1; tries to get value out of storage if there is one
-            updateActiveStep((parseInt(localStorage.fossContribGuideStep) || 1) - 1);
-        } else {
-            updateActiveStep(0);
-        }
-    };
-    
-    $('.checkbox-clickable').live('click', function(e) {
-        var id;
-        e.preventDefault();
-        
-        $(this).toggleClass('checked');
-        if ($(this).hasClass('checked')) { 
-            $(this).closest('li').addClass('completed-step');
-            updateActiveStep(1); 
-        } else { 
-            $(this).closest('li').removeClass('completed-step');
-            id = $(this).closest('li').attr('id');
-            updateActiveStep((activeStepNum - getStepNumber(id)) * -1); 
-        }
-    });
-    
-    updateActiveStep = function(stepCount) {
-        // stepCount will usually be 1
-        // to go backwards, stepCount can be negative
-        var oldStep,
-            newStep,
-            oldActiveStepNum;
-        
-        // console.log(activeStepNum);
-        
-        oldStep = $('.active-step');
-        $(oldStep).removeClass('active-step');
-        
-        oldActiveStepNum = activeStepNum;
-        activeStepNum = activeStepNum + stepCount;
-        
-        // handler for fixing multiple steps
-        if (oldActiveStepNum > activeStepNum) {
-            // going backward
-            while (oldActiveStepNum != activeStepNum) {
-                $('#' + getStepID(oldActiveStepNum))
-                    .removeClass('completed-step')
-                    .find('.checkbox')
-                    .removeClass('checked')
-                    .removeClass('checkbox-clickable');
-                    
-                oldActiveStepNum--;
-            }
-        } else {
-            // going forward
-            while (oldActiveStepNum != activeStepNum) {
-                $('#' + getStepID(oldActiveStepNum))
-                    .addClass('completed-step')
-                    .find('.checkbox')
-                    .addClass('checkbox-clickable')
-                    .addClass('checked');
-                    
-                oldActiveStepNum++;
-            }
-        }
-        
-        // console.log(activeStepNum);
+// very basic document.getElementsByClassName polyfill
+if (!document.getElementsByClassName) {
+  document.getElementsByClassName = function (el) {
+    document.body.querySelectorAll('.'+el)
+  };
+}
 
-        newStep = $('#' + getStepID(activeStepNum));
+// https://developer.mozilla.org/en-US/docs/DOM/Storage
+// very basic localStorage polyfill
+if (!window.localStorage) {
+  window.localStorage = {
+    getItem: function (sKey) {
+      if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
+      return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+    },
+    key: function (nKeyId) {
+      return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
+    },
+    setItem: function (sKey, sValue) {
+      if(!sKey) { return; }
+      document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+      this.length = document.cookie.match(/\=/g).length;
+    },
+    length: 0,
+    removeItem: function (sKey) {
+      if (!sKey || !this.hasOwnProperty(sKey)) { return; }
+      document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+      this.length--;
+    },
+    hasOwnProperty: function (sKey) {
+      return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    }
+  };
+  window.localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
+}
 
-        $(newStep)
-            .addClass('active-step')
-            .find('.checkbox')
-            .removeClass('.checked')
-            .addClass('checkbox-clickable');
-            
-        if (canHazLocalStorage) {
-            localStorage.fossContribGuideStep = activeStepNum;
-            console.log(localStorage.fossContribGuideStep);
-        }
-    };
-    
-    getStepID = function(num) {
-        return 'step-' + num;
-    };
-    
-    getStepNumber = function(id) {
-        // ids are 'step-*'
-        return parseInt(id.slice(5));
-    };
-
-    init();
-});
-
-var supports_html5_storage = function() {
-  try {
-    return 'localStorage' in window && window['localStorage'] !== null;
-  } catch (e) {
-    return false;
+function updateStepsStatus() {
+  var steps = document.getElementsByClassName('step');
+  var storedStepNumber = function () { return parseInt(window.localStorage.getItem('stepNumber'),10) };
+  for(var i=0,im=steps.length; i<im; i++) {
+    steps[i].className = 'step ' + (function () {
+      if(i < storedStepNumber()) { return ' past'; }
+      if(i === storedStepNumber()) { return ' current'; }
+      if(i > storedStepNumber()) { return ' futur'; }
+    })();
   }
 }
+
+function toogleStep(event) {
+  var step = event.currentTarget.parentNode.parentNode;
+  var stepNumber = parseInt(step.id.match(/[0-9]+$/)[0], 10);
+  var storedStepNumber = function () { return parseInt(window.localStorage.getItem('stepNumber'),10) };
+  if( stepNumber === storedStepNumber() ) {
+    window.localStorage.setItem('stepNumber', stepNumber + 1)
+    updateStepsStatus();
+  }
+  else if( stepNumber < storedStepNumber() ) {
+    window.localStorage.setItem('stepNumber', stepNumber)
+    updateStepsStatus();
+  }
+}
+
+function init() {
+  var storedStepNumber = function () { return parseInt(window.localStorage.getItem('stepNumber'),10) };
+  // load stepNumber to localStorage
+  window.localStorage.setItem( 'stepNumber', storedStepNumber() || 0 );
+
+  // add checkboxes to the document
+  var container,
+  title,
+  checkbox,
+  steps = document.getElementsByClassName('step');
+  for(var i=0,im=steps.length; i<im; i++) {
+    steps[i].id = 'step' + i;
+    container = document.createElement('h2');
+    title = document.createElement('span');
+    // replace the former h2 tag by a new empty one,
+    // puts its content into the title variable.
+    title.innerHTML = steps[i].replaceChild(
+      container,
+      steps[i].getElementsByTagName('h2')[0]
+    ).innerHTML;
+    checkbox = document.createElement('span');
+    checkbox.className = 'checkbox';
+    checkbox.addEventListener('click', toogleStep, false);
+    container.appendChild(checkbox);
+    container.appendChild(title);
+  }
+
+  updateStepsStatus();
+}
+
+// Run !
+init();
+
